1d68aa975c853ef12db50f1b638278f6178271a7
Ashwin Somasundara <ashwin.somasundara@gmail.com>
2016-11-29 12:44:49 -0500

    Add files via upload

--- ListPerformance.py
+++ ListPerformance.py
@@ -1,5 +1,6 @@
 from timeit import Timer
 
+
 #Concatenate
 def test1():
     l = []
@@ -28,3 +29,28 @@ print("comprehension ",t3.timeit(number=1000), "milliseconds")
 t4 = Timer("test4()", "from __main__ import test4")
 print("list range ",t4.timeit(number=1000), "milliseconds")
    
+popzero = timeit.Timer("x.pop(0)",
+                       "from __main__ import x")
+					   
+popend = timeit.Timer("x.pop()",
+                      "from __main__ import x")
+
+print("pop(0)   pop()")
+for i in range(1000000,100000001,1000000):
+    x = list(range(i))
+    pt = popend.timeit(number=1000)
+    x = list(range(i))
+    pz = popzero.timeit(number=1000)
+    print("%15.5f, %15.5f" %(pz,pt))
+
+import timeit
+import random
+
+for i in range(10000,1000001,20000):
+    t = timeit.Timer("random.randrange(%d) in x"%i,
+                     "from __main__ import random,x")
+    x = list(range(i))
+    lst_time = t.timeit(number=1000)
+    x = {j:None for j in range(i)} #Create dictionary
+    d_time = t.timeit(number=1000)
+    print("%d,%10.3f,%10.3f" % (i, lst_time, d_time))
