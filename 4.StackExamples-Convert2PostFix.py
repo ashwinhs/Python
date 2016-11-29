04d384315006eeff9a359c522fa6b7a72358b5dc
Ashwin Somasundara <ashwin.somasundara@gmail.com>
2016-11-29 18:03:32 -0500

    Add files via upload

---
+++ 4.StackExamples-Convert2PostFix.py
@@ -0,0 +1,39 @@
+class Stack:
+	def __init__(self):
+		self.list = []
+	
+	def push(self, obj):
+		self.list.append(obj)
+	
+	def pop(self):
+		return self.list.pop()
+	
+	def peek(self):
+		return self.list[len(self.list) - 1]
+	
+	def isEmpty(self):
+		return self.list == []
+	
+	def size(self):
+		return len(self.list)
+
+def convertToPostFix(string):
+	finalString = ''
+	wordList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+	opsList = '+,/*'
+	s = Stack()
+	for i in string:
+		if i in wordList:
+			finalString += i
+		if i in opsList:
+			if s.isEmpty():
+				s.push(i)
+			elif opsList.find(s.peek()) >= opsList.find(i):
+				finalString += s.pop()
+				s.push(i)
+			else:
+				finalString += i
+		if i == ')'
+			finalString += s.pop()
+	while not s.isEmpty():
+		finalString += s.pop()
